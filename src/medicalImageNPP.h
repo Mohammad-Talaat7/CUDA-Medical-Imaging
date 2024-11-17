38cd282bed83cd95672e80684c41eb836043adc4
Mohammad Talaat <mohammadblackid2@gmail.com>
2024-11-17 04:08:55 +0200

    Adding the Repo

---
+++ src/medicalImageNPP.h
@@ -0,0 +1,25 @@
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#define WINDOWS_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#pragma warning(disable : 4819)
+#endif
+
+#include <npp.h>
+#include <cuda_runtime.h>
+#include <iostream>
+
+#include <Exceptions.h>
+#include <ImageIO.h>
+#include <ImagesCPU.h>
+#include <ImagesNPP.h>
+
+#include <string.h>
+#include <fstream>
+#include <iostream>
+
+#include <cuda_runtime.h>
+#include <npp.h>
+
+#include <helper_cuda.h>
+#include <helper_string.h>
